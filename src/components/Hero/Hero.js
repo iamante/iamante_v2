f540094c9c4cdf0e40c09c8f61188d90d2abb65f
iamante <amantejamse@gmail.com>
2021-09-07 21:54:54 +0800

    fixing font-size

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 function Hero() {
-    return (
-        <section className="hero">
-            <div className="hero-title">
-                <h4>Ahoy! I'm</h4>
-                <div>James</div>
-                <div>Amante</div>
-                <h4>A web developer.</h4>
-            </div>
-            <img src="images/cloud.png" alt="black-cloud" />
-            <img src="images/light1.png" alt="lightning" id="light1" />
-            <img src="images/light2.png" alt="lightning" id="light2" />
-            <img src="images/cloud-f.png" alt="black-cloud-front"/>
-        </section>
-    )
+  return (
+    <section className="hero">
+      <div className="hero-title">
+        <h4>Ahoy! I'm</h4>
+        <h1>James <span>Amante</span></h1>
+        <span>A web developer.</span>
+      </div>
+      <img src="images/cloud.png" alt="black-cloud" />
+      <img src="images/light1.png" alt="lightning" id="light1" />
+      <img src="images/light2.png" alt="lightning" id="light2" />
+      <img src="images/cloud-f.png" alt="black-cloud-front" />
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
