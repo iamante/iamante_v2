e7699f01d84d55b936a11fc6e79107f3f311184e
James Amante <77043207+iamante@users.noreply.github.com>
2024-12-20 01:35:46 +0800

    Revert "Add linkedin badge"
    This reverts commit 27cdf1faa369d1ad24b2998c164a2cfea1cdd1a1.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,6 @@ function About() {
                 <h1>Hey, I'm James<br/> a Software Engineer</h1>
                 <h4>From San jose del monte Bulacan, Philippines.</h4>
                 <p>I have a passion in technology and programming learning new things always give me excitement as there will always challenge to overcome. I'm a self taught developer strive to become a great developer that i can be, so you will find me learning and reading about coding and other stuff .</p>
-                <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="iamante" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ph.linkedin.com/in/iamante?trk=profile-badge">James Amante</a></div>
             </div>
             <div className="about-img">
                 <img src="images/james.png" alt="james" draggable="false"/>
