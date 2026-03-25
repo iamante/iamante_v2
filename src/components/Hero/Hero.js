20078a070ed8145867f1bc5ff79afa8dafe58467
James Amante <77043207+iamante@users.noreply.github.com>
2026-03-26 00:00:45 +0800

    Update Hero.js with pirate boat GSAP animations

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,29 @@
-import React from "react";
-import "./Hero.css";
+// Pirate Boat with GSAP Animations
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 
-function Hero() {
+const Hero = () => {
+  useEffect(() => {
+    // Initialize GSAP animations
+    const boat = document.querySelector('.boat');
+    gsap.to(boat, {
+      duration: 2,
+      x: 100,
+      rotation: 360,
+      repeat: -1,
+      yoyo: true,
+    });
+  }, []);
+
   return (
-    <section className="hero" id="hero">
-      <div className="hero-title">
-        <h1>
-          <span>Ahoy! I'm</span> James <span>Amante</span>
-          <span>A software engineer.</span>
-        </h1>
+    <div className="hero">
+      <h1>Welcome to the Pirate Adventure!</h1>
+      <div className="boat">
+        {/* Render your pirate boat graphic here */}
+        <img src="/path/to/pirate-boat.png" alt="Pirate Boat" />
       </div>
-      <img src="images/cloud.png" alt="black-cloud" draggable="false"/>
-      <img src="images/light1.png" alt="lightning" id="light1" draggable="false"/>
-      <img src="images/light2.png" alt="lightning" id="light2" draggable="false"/>
-      <img src="images/cloud-f.png" alt="black-cloud-front" draggable="false"/>
-    </section>
+    </div>
   );
-}
+};
 
 export default Hero;
